86ddf0c0286b6f4eaa1d057524529e7f9c9d7b85
Brandon Molyneaux <bdm0041@tigermail.auburn..edu>
2020-02-09 11:28:20 -0600

    final commit for the hackathon. time for sleep

--- studentweb/assignment_manager/views.py
+++ studentweb/assignment_manager/views.py
@@ -5,7 +5,19 @@ from .models import Assignments
 
 def assignmentHTML(request):
     assignment_list = Assignments.objects.all()
-    print("assignment list: ")
-    for i in assignment_list:
-        print(i.name)
     return render(request, "assignment_manager/assignment_manager.html", {'assignments' : assignment_list})
+
+def create_assignment(request):
+        if request.method == 'submit':
+            if request.POST.get('name') and request.POST.get('duedate'):
+                post=Assignments()
+                post.name= request.POST.get('name')
+                post.due_date= request.POST.get('duedate')
+                post.save()
+                
+                return render(request, 'assignment_manager.html')  
+
+        else:
+                print("failed")
+                return render(request,'assignment_manager.html')
+
