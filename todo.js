ca18e8ce7612ec64918ab12c2f508a92812edc40
Tae <tm@Taes-air.com>
2020-02-08 21:44:59 -0600

    Created todo.html

---
+++ todo.js
@@ -0,0 +1,17 @@
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<script type="text/javascript" src="dist/js/propeller.min.js"></script>
+
+<script type="text/javascript" language="javascript" src="datetimepicker/js/moment-with-locales.js"></script>
+
+<script type="text/javascript" language="javascript" src="datetimepicker/js/bootstrap-datetimepicker.js"></script>
+
+<script>
+	// Default date and time picker
+	$('#datetimepicker-default').datetimepicker();
+</script>
